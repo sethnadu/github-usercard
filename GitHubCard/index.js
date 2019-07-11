bc2f7eca8ec8c04042b6835f4456b2355189209a
Seth Nadu <seth.nadu@gmail.com>
2019-07-11 14:54:57 -0400

    got my profile setup

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,30 @@
            https://api.github.com/users/<your name>
 */
 
+const cards = document.querySelector(".cards");
+
+const seth = axios.get("https://api.github.com/users/sethnadu");
+
+console.log(seth);
+seth 
+  .then(data => {
+    const img = data.data.avatar_url;
+    const name = data.data.name;
+    const username = data.data.login;
+    const location = data.data.location;
+    const address = data.data.url;
+    const followersCount = data.data.followers;
+    const followingCount = data.data.following;
+    const bio = data.data.bio;
+
+    console.log(username)
+
+    const sethNadu = createGitHubCards(img, name, username, location, address, followersCount, followingCount, bio);
+
+    cards.appendChild(sethNadu);
+  })
+
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -45,6 +69,48 @@ const followersArray = [];
 </div>
 
 */
+
+
+function createGitHubCards(img, name, username, location, address, followersCount, followingCount, bio) {
+
+const cardDiv = document.createElement("div");
+const userImg = document.createElement('img');
+const cardInfoDiv = document.createElement("div");
+const usersName = document.createElement("h3");
+const userName = document.createElement("p");
+const userLocation= document.createElement("p");
+const userAddress = document.createElement("p");
+const userFollowers = document.createElement("p");
+const userFollowing = document.createElement("p");
+const userBio = document.createElement("p");
+
+cardDiv.classList.add("card");
+cardInfoDiv.classList.add("card-info");
+usersName.classList.add("name");
+userName.classList.add("username");
+
+cardDiv.appendChild(userImg);
+cardDiv.appendChild(cardInfoDiv);
+cardInfoDiv.appendChild(usersName);
+cardInfoDiv.appendChild(userName);
+cardInfoDiv.appendChild(userLocation);
+cardInfoDiv.appendChild(userAddress);
+cardInfoDiv.appendChild(userFollowers);
+cardInfoDiv.appendChild(userFollowing);
+cardInfoDiv.appendChild(userBio);
+
+userImg.src = img;
+usersName.textContent = name;
+userName.textContent = username;
+userLocation.textContent = location;
+userAddress.textContent = address;
+userFollowers.textContent = followersCount;
+userFollowing.textContent = followingCount;
+userBio.textContent = bio;
+
+return cardDiv;
+
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
